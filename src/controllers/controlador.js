ab7259335989e42bcec47331b98c0d2701a8e159
amomishijos1152193974 <stiven.vlz2191@gmail.com>
2022-07-21 17:42:50 -0500

    se modifica el index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+// CREANDO BARIABLES 
+let nombreUsuario="Stiven Velez"
+const EDAD_USUARIO=30
+let estaturaUsuario=1.67
+let hinchaDelReal=true
+
+console.log(nombreUsuario)
+
+
+console.log(EDAD_USUARIO)
+
